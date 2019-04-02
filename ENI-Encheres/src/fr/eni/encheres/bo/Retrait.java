bf8c8ab62a8edc4c55bcc2e99029af590cd026af
rdanglos2018 <rdanglos2018@27-UC37-14.ad.campus-eni.fr>
2019-04-02 14:12:26 +0200

    Modif classe retrait

--- ENI-Encheres/src/fr/eni/encheres/bo/Retrait.java
+++ ENI-Encheres/src/fr/eni/encheres/bo/Retrait.java
@@ -1,6 +1,7 @@
 package fr.eni.encheres.bo;
 
 public class Retrait {
+	private int no_article;
 	private String rue;
 	private String code_postal;
 	private String ville;
@@ -9,11 +10,12 @@ public class Retrait {
 	 * @param code_postal
 	 * @param ville
 	 */
-	public Retrait(String rue, String code_postal, String ville) {
+	public Retrait(int no_article,String rue, String code_postal, String ville) {
 		super();
 		this.rue = rue;
 		this.code_postal = code_postal;
 		this.ville = ville;
+		this.no_article = no_article;
 	}
 	public String getRue() {
 		return rue;
@@ -32,6 +34,12 @@ public class Retrait {
 	}
 	public void setVille(String ville) {
 		this.ville = ville;
+	}
+	public int getNo_article() {
+		return no_article;
+	}
+	public void setNo_article(int no_article) {
+		this.no_article = no_article;
 	}
 	
 	
