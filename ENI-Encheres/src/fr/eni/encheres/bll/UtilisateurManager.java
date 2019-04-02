eb0ffb9f965a40eec0f8617dcb5b0cac2f3c80f3
rdanglos2018 <rdanglos2018@27-UC37-14.ad.campus-eni.fr>
2019-04-02 09:24:07 +0200

    Robyn : BLL DAL Utilisateur + Fichier de test DAL init

---
+++ ENI-Encheres/src/fr/eni/encheres/bll/UtilisateurManager.java
@@ -0,0 +1,43 @@
+package fr.eni.encheres.bll;
+
+import java.util.List;
+
+import fr.eni.encheres.BusinessException;
+import fr.eni.encheres.bo.Utilisateur;
+import fr.eni.encheres.dal.DAOFactory;
+import fr.eni.encheres.dal.UtilisateurDAO;
+
+public class UtilisateurManager {
+	
+	private UtilisateurDAO utilisateurDAO;
+	
+	public UtilisateurManager() {
+		utilisateurDAO = DAOFactory.getUtilisateurDAO();
+	}
+	
+	public List<Utilisateur> selectionnerUtilisateurs() throws BusinessException{
+		return utilisateurDAO.selectAll();
+	}
+	
+	public Utilisateur selectionnerUtilisateur(int idUtilisateur) throws BusinessException {
+		return utilisateurDAO.selectById(idUtilisateur);
+	}
+
+	public Utilisateur ajouterUtilisateur(int noUtilisateur, String pseudo, String nom, String prenom, String email, String telephone,
+			String rue, String codePostal, String ville, String motDePasse, int credit, String administrateur) throws BusinessException {
+		Utilisateur nouvelUtilisateur = new Utilisateur(noUtilisateur, pseudo, nom, prenom, email, telephone, rue, codePostal, ville, motDePasse, credit, administrateur);
+		utilisateurDAO.insert(nouvelUtilisateur);
+		return nouvelUtilisateur;
+	}
+	
+	public void supprimerUtilisateur(int idUtilisateur) throws BusinessException{
+		utilisateurDAO.delete(idUtilisateur);
+	}
+	
+	public void updateUtilisateur(int noUtilisateur, String pseudo, String nom, String prenom, String email, String telephone,
+			String rue, String codePostal, String ville, String motDePasse, int credit, String administrateur) throws BusinessException {
+		Utilisateur utilisateurAModifier = new Utilisateur(noUtilisateur, pseudo, nom, prenom, email, telephone, rue, codePostal, ville, motDePasse, credit, administrateur);
+		utilisateurDAO.update(utilisateurAModifier);
+	}
+	
+}
---
+++ ENI-Encheres/src/fr/eni/encheres/dal/TestDal.java
@@ -0,0 +1,16 @@
+package fr.eni.encheres.dal;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import fr.eni.encheres.bo.Utilisateur;
+
+public class TestDal {
+
+	private List<Utilisateur> listUser = new ArrayList<Utilisateur>();
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
