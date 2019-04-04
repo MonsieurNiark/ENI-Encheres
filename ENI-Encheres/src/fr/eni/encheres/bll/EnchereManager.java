8aeb867fc1c27d1cc7a47dbad27bc2ac9322492e
acoskun2018 <acoskun2018@27-UC37-07.ad.campus-eni.fr>
2019-04-04 10:21:38 +0200

    Alex : J'ai oublié un Manager

---
+++ ENI-Encheres/src/fr/eni/encheres/bll/EnchereManager.java
@@ -0,0 +1,43 @@
+package fr.eni.encheres.bll;
+
+import java.util.List;
+import java.util.Date;
+
+import fr.eni.encheres.BusinessException;
+import fr.eni.encheres.bo.ArticleVendu;
+import fr.eni.encheres.bo.Enchere;
+import fr.eni.encheres.bo.Utilisateur;
+import fr.eni.encheres.dal.DAOFactory;
+import fr.eni.encheres.dal.EncheresDAO;
+
+public class EnchereManager {
+
+	private EncheresDAO encheresDAO;
+	
+	public EnchereManager() {
+		encheresDAO = DAOFactory.getEncheresDAO();
+	}
+	
+	public List<Enchere> selectionnerEnchere() throws BusinessException {
+		return encheresDAO.selectAll();
+	}
+	
+	public Enchere selectionnerEnchereParIdUser(int IdUser) throws BusinessException{
+		return encheresDAO.selectByUser(IdUser);
+	}
+	
+	public Enchere selectionnerEnchereParIdArticle(int IdArticle) throws BusinessException{
+		return encheresDAO.selectByArticle(IdArticle);
+	}
+	
+	public void supprimerEnchere(int Id) throws BusinessException{
+		encheresDAO.delete(Id);
+	}
+	
+	
+	public Enchere insererEnchere(int id, Date date_enchere, float montant_enchere, Utilisateur util, ArticleVendu art) throws BusinessException{
+		Enchere nouvelEnchere = new Enchere(id, date_enchere, montant_enchere, util, art);
+		encheresDAO.insert(nouvelEnchere);
+		return nouvelEnchere;
+	}
+}
