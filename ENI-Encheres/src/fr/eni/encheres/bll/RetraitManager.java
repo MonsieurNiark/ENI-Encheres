32b8d34ce6a8e05d0f5798056f43ee6c6f3bcfd2
acoskun2018 <acoskun2018@27-UC37-07.ad.campus-eni.fr>
2019-04-10 12:25:12 +0200

    Alex : retrait fix

--- ENI-Encheres/src/fr/eni/encheres/bll/RetraitManager.java
+++ ENI-Encheres/src/fr/eni/encheres/bll/RetraitManager.java
@@ -1,6 +1,7 @@
 package fr.eni.encheres.bll;
 
 import fr.eni.encheres.BusinessException;
+import fr.eni.encheres.bo.ArticleVendu;
 import fr.eni.encheres.bo.Retrait;
 import fr.eni.encheres.dal.DAOFactory;
 import fr.eni.encheres.dal.RetraitDAO;
@@ -17,8 +18,8 @@ public class RetraitManager {
 		return retraitDAO.selectById(idRetrait);
 	}
 	
-	public Retrait ajouterRetrait(int no_article, String rue, String code_postal, String ville) throws BusinessException {
-		Retrait nouveauRetrait = new Retrait(no_article, rue, code_postal, ville);
+	public Retrait ajouterRetrait(int no_Retrait, ArticleVendu no_article, String rue, String code_postal, String ville) throws BusinessException {
+		Retrait nouveauRetrait = new Retrait(no_Retrait, no_article, rue, code_postal, ville);
 		retraitDAO.insert(nouveauRetrait);
 		return nouveauRetrait;
 	}
@@ -27,8 +28,8 @@ public class RetraitManager {
 		retraitDAO.delete(no_article);
 	}
 	
-	public void updateRetrait(int no_article, String rue, String code_postal, String ville) throws BusinessException {
-		Retrait retraitAModifier = new Retrait(no_article, rue, code_postal, ville);
+	public void updateRetrait(int no_Retrait, ArticleVendu no_article,String rue, String code_postal, String ville) throws BusinessException {
+		Retrait retraitAModifier = new Retrait(no_Retrait,no_article, rue, code_postal, ville);
 		retraitDAO.update(retraitAModifier);
 	}
 }
