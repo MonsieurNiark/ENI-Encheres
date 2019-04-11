60350b9a184cc991d0c3c5b99f7893c95e2ef1a0
rdanglos2018 <rdanglos2018@27-UC37-14.ad.campus-eni.fr>
2019-04-11 09:46:57 +0200

    New request Enchere

--- ENI-Encheres/src/fr/eni/encheres/dal/EncheresDAOjdbclmpl.java
+++ ENI-Encheres/src/fr/eni/encheres/dal/EncheresDAOjdbclmpl.java
@@ -33,9 +33,21 @@ public class EncheresDAOjdbclmpl implements EncheresDAO{
 	private static final String DELETE_VENTE = "DELETE * FROM ENCHERES WHERE no_enchere=?";
 	private static final String SELECT_LAST_ENCHERE = SELECT_ALL + " WHERE ENCHERES.no_article=? AND montant_enchere = (SELECT max(montant_enchere) FROM encheres where no_article=?)";
 	private static final String SELECT_LAST_ENCHERE_FROM_USER = SELECT_ALL + " WHERE ENCHERES.no_article=? AND ENCHERES.no_utilisateur=? AND montant_enchere = (SELECT max(montant_enchere) FROM encheres where no_utilisateur=? and no_article=?)";
-	private static final String SELECT_LAST_ENCHERE_FOREACH_USER = "SELECT DISTINCT ENCHERES.no_utilisateur, MAX(montant_enchere) as montant_enchere FROM ENCHERES" + 
-			" INNER JOIN ARTICLES_VENDUS on ENCHERES.no_article = ARTICLES_VENDUS.no_article" + 
-			" WHERE ENCHERES.no_article = 1 and etat=0 GROUP BY ENCHERES.no_utilisateur ";
+	private static final String SELECT_LAST_ENCHERE_FOREACH_USER = "SELECT DISTINCT ENCHERES.no_utilisateur,MAX(montant_enchere) as montant_enchere, ENCHERES.no_article," + 
+			"			 ARTICLES_VENDUS.no_article, nom_article, description, date_debut_encheres, date_fin_encheres, prix_initial, prix_vente," + 
+			"			 ARTICLES_VENDUS.no_utilisateur, ARTICLES_VENDUS.no_categorie, " + 
+			"			 UTILISATEURS.no_utilisateur, pseudo, nom, prenom, email, telephone, rue, code_postal, ville, mot_de_passe," + 
+			"			 credit, administrateur," + 
+			"		 CATEGORIES.no_categorie, CATEGORIES.libelle " + 
+			"		 FROM ENCHERES" + 
+			"			 INNER JOIN ARTICLES_VENDUS ON ENCHERES.no_article          = ARTICLES_VENDUS.no_article" + 
+			"			 INNER JOIN CATEGORIES      ON ARTICLES_VENDUS.no_categorie = CATEGORIES.no_categorie" + 
+			"			 INNER JOIN UTILISATEURS    ON ENCHERES.no_utilisateur      = UTILISATEURS.no_utilisateur" + 
+			"			  WHERE ENCHERES.no_article = 1 and etat=0 GROUP BY ENCHERES.no_utilisateur, " + 
+			"			  ENCHERES.no_article,ARTICLES_VENDUS.no_article,ARTICLES_VENDUS.nom_article,ARTICLES_VENDUS.description,\r\n" + 
+			"			  ARTICLES_VENDUS.date_debut_encheres,ARTICLES_VENDUS.date_fin_encheres,ARTICLES_VENDUS.prix_initial,ARTICLES_VENDUS.prix_vente,ARTICLES_VENDUS.no_utilisateur," + 
+			"			  ARTICLES_VENDUS.no_categorie,UTILISATEURS.no_utilisateur,UTILISATEURS.pseudo,UTILISATEURS.nom,prenom,email,telephone,rue,code_postal,ville,mot_de_passe,credit,administrateur," + 
+			"			  CATEGORIES.no_categorie,CATEGORIES.libelle";
 	
 	@Override
 	public void insert(Enchere enchere) throws BusinessException {
@@ -228,6 +240,9 @@ public class EncheresDAOjdbclmpl implements EncheresDAO{
 		
 		return result;
 	}
+	
+
+	
 	
 	@Override
 	public Enchere selectLastEnchereByArticle(int id) throws BusinessException {
